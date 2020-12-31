9d70028e16859789bc473250c9360d8aee8d0b6f
Harsith Priyan Sivanandham <harsith-developer@Harsiths-MacBook-Pro.local>
2020-12-31 21:31:59 +0530

    Add a text box to get user input

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivy.uix.button import Button
+from kivymd.uix.textfield import MDTextField
+from kivy.lang import Builder
 import socket
 import os
+
+username_helper = """ 
+MDTextField:
+    hint_text: "Enter Username"
+    hint_helper: "Or hit forgot username"
+    helper_text_mode: "on_focus"
+    pos_hint: {'center_x':0.5, 'center_y':0.5}
+    size_hint_x:None
+    width:500
+ """
+
+class main(MDApp):
+    def build(self):
+        screen = Screen()
+        #username = MDTextField(text='Enter Username',pos_hint={'center_X':0.5,'center_y':0.5})
+        username = Builder.load_string(username_helper)
+        screen.add_widget(username)
+        return screen
+
+main().run()
